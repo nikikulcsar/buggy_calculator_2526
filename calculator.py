f8f4c6ceaeb35983e01ce31cca22e79ca48e03cb
Kulcsár Nikolett <niki.kulcsar.97@gmail.com>
2026-04-19 21:56:59 +0200

    Fix subtract, multiply and divide logic

--- calculator.py
+++ calculator.py
@@ -3,14 +3,14 @@ def add(a, b):
 
 
 def subtract(a, b):
-    return abs(a - b)
+    return a - b
 
 
 def multiply(a, b):
-    return int(a) * b
+    return a * b
 
 
 def divide(a, b):
     if b == 0:
-        return 0
+        raise ZeroDivisionError("Cannot divide by zero")
     return a / b
